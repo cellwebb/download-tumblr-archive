adb930deca767fc01d05fa0815e824247b9ce3ea
thomwebb <webb.thomasj@gmail.com>
2015-05-09 00:14:52 -0500

    like a phoenix, saved from an overwrite

--- getpics.py
+++ getpics.py
@@ -13,88 +13,114 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import sys
+import csv
 
-import unittest, time, re
-
-
+import time
 
 script, tumblr_blog = argv
 
 tumblr_url = 'http://' + tumblr_blog + '.tumblr.com'
 
 
+print(chr(27) + "[2J") # clear terminal
+
+scrolling = False
+downloading = False
+
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
-verificationErrors = []
-accept_next_alert = True
-#time.sleep(10)
 driver.get(tumblr_url)
-time.sleep(10)
-driver.find_element("id","posts-wrapper").click()
-print "clicking..."
-time.sleep(10)
-scrolls = 2
-for i in range(0,scrolls):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    print "scrolling... (%r\%)" % ((i+1)/scrolls)
-    time.sleep(4)
+if scrolling == True:
+    delay = 3
+    for i in range(delay):
+        print i
+        time.sleep(1)
+    driver.find_element("id","posts-wrapper").click()
+    print "clicking..."
+    delay = 3
+    for i in range(delay):
+        print i
+        time.sleep(1)
+    scrolls = 2
+    for i in range(0,scrolls):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        percent = (i + 1)*100/scrolls
+        print "scrolling... (%s)" % (percent)
+        time.sleep(2)
 tumblr_html = driver.page_source
-#print tumblr_html
 driver.quit()
-
 
 download_folder = 'downloadedpics/'
 
 soup = BeautifulSoup(tumblr_html)
 
 image_urls = []
-# usernames = []
-# descriptions = []
-# photosets = []
+usernames = []
+descriptions = []
+photosets = []
 
 #extract all image URLs with special rules for photosets
 for link in soup.find_all('img'):
     if 'px.srvcs' in link.get('src'):
         # skip any urls from 'pixel services'
+        print "px.srvcs avoided"
+        continue
+    if '.gif' in link.get('src'):
+        # skip over the ajax loader
         continue
 
     image_url = link.get('src')
-    print image_url
-    print type(image_url)
 
-    # if 'photoset' in link.parent:
-    #     caption = link.find_next("div", "captions")
-    #     username = caption.a.get_text()
-    #     description = caption.blockquote.find_all('p')
-    #     part_of_a_set = True
-    # else:
-    #     caption = link.get('alt')
-    #     if type(caption) == "NoneType":
-    #         caption = link.get('title')
-    #     username = caption.split(':')[0]
-    #     description = caption.split(':')[1].strip()
-    #     part_of_a_set = False
+    caption = ''
+    username = ''
+    description = ''
+    part_of_a_set = False
+
+    try:
+        if 'photoset-row' in link.parent.parent.get('class'):
+            part_of_a_set = True
+        else:
+            if 'normal-width' in link.parent.get('class'):
+                # under the assumption that normal-width images
+                # always come with larger-width versions, we'll
+                # skip the normal-width ones
+                continue
+        caption = link.find_next('div', 'captions')
+        username = caption.p.a.get_text()
+        description = caption.blockquote.get_text()
+    except TypeError:
+        if 'instagram' in link.parent.parent.parent.get('id'):
+            if link.has_attr('title'):
+                caption = link.get('title')
+            else:
+                print "instagram, no title", link
+        try:
+            if '@' in caption:
+                username = caption.split('@')[1]
+                description = caption.split('@')[0]
+            else:
+                # print "no :, no @", link
+                username = "tumblr_blog"
+                description = caption
+        except IndexError:
+            print "IndexError"
+            # print caption
+            # print link
+            # print link.parent
+            # sys.exit()
 
     image_urls.append(image_url)
-    # usernames.append(username)
-    # descriptions.append(description)
-    # photosets.append(part_of_a_set)
-
-for indexA in xrange(len(image_urls) - 1, 0, -1): # counts down from the top so items can be removed correctly
-    if '_1280' in image_urls[indexA]:
-        continue
-    imagefilenameA = image_urls[indexA].split('/')[-1]
-    # for indexB in xrange(0, len(image_urls) - 1, 1):
-    #     imagefilenameB = image_urls[indexB].split('/')[-1]
-    #     if (imagefilenameA.replace('_500', '_1280') == imagefilenameB) & (imagefilenameA != imagefilenameB):
-    #         # delete lower pixel duplicates
-    #         image_urls.pop(indexA)
-    #         print "POP!"
-            # usernames.pop(indexA)
-            # descriptions.pop(indexA)
-            # photosets.pop(indexA)
+    usernames.append(username)
+    descriptions.append(description)
+    photosets.append(part_of_a_set)
 
 for image_url in image_urls:
     print image_url
-    # download pictures
-    urlretrieve(image_url, download_folder + image_url.split('/')[-1])
+
+for description in descriptions:
+    print description
+
+if downloading:
+    print "Now downloading %r images." % (len(image_urls))
+    for image_url in image_urls:
+        urlretrieve(image_url, download_folder + image_url.split('/')[-1])
